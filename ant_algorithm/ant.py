54345b1a2dc36981cc6d522d31ea86cae59db5eb
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-11-29 13:16:40 +0800

    upgrade ant algorithm

--- ant_algorithm/ant.py
+++ ant_algorithm/ant.py
@@ -25,7 +25,7 @@ class AntAlgorithm:
         # Инициализация феромонов для всех ребер графа с минимальным уровнем 0.01
         self.pheromones = {(node, neighbour): 0.01 for node in self.graph.nodes for neighbour, _ in node.nb_begin()}
         
-        # Списки для хранения данных о прогрессе оптимизации
+        # Списки для хранения данных о прогрессе
         self.iterations = []  # Номер итерации
         self.distances = []   # Лучшие расстояния на каждой итерации
 
@@ -51,52 +51,120 @@ class AntAlgorithm:
         max_no_improvement = self.num_iterations  # Порог для завершения алгоритма
 
         # Настройка интерактивной визуализации
-        plt.ion()
-        fig, ax = plt.subplots()
+        # plt.ion()
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 18))
 
         iteration = 0
+        probabilities = []  # Массив для хранения вероятностей
+        min_paths = []  # Массив для хранения минимальных 
+        all_pheromones = []
+
         while no_improvement_count < max_no_improvement:
             iteration += 1
             all_cycles = []  # Список маршрутов, построенных всеми муравьями
             improved = False
 
-            # Каждому муравью поручается построение маршрута
+            # Цикл для муравьев
             for _ in range(self.num_ants):
                 cycle, distance = self.construct_solution()
                 
-                # Если найден новый лучший маршрут, обновляем данные
+                # Обновляем данные
                 if distance < best_distance:
                     best_distance = distance
                     best_cycle = cycle
                     print(f"New best distance found: {best_distance} at iteration {iteration}")
                     improved = True
+                
                 if cycle is not None:
                     all_cycles.append((cycle, distance))
-            
-                    # Обновление уровня феромонов на основании всех построенных маршрутов
-                    self.update_pheromones(all_cycles)
+                    self.update_pheromones(cycle, distance)
+
             if not improved:
                 no_improvement_count += 1
             else:
                 no_improvement_count = 0  # Сброс счетчика при улучшении
-            # Запись данных для построения графика
+            # Испарение феромонов и запись данных для графиков
+            self.evaporate_pheromones()
+            all_pheromones.append(self.pheromones.copy())
             self.iterations.append(iteration)
             self.distances.append(best_distance)
+            if iteration>1:
+                min_distance_in_iteration = min([distance for _, distance in all_cycles]) if all_cycles else min_paths[-1]
+            else:
+                min_distance_in_iteration = None
+            min_paths.append(min_distance_in_iteration)  # Добавляем текущую минимальную длину пути
+            # Обновление вероятности выбора лучшего маршрута
+            probability_of_best_path = self.calculate_probability_of_best_path(best_cycle, self.pheromones)
+            probabilities.append(probability_of_best_path)
 
-            # Обновление графика визуализации
-            ax.clear()
-            ax.plot(self.iterations, self.distances, label='Best Distance')
-            ax.set_xlabel('Iteration')
-            ax.set_ylabel('Distance')
-            ax.set_title('Ant Algorithm Optimization')
-            ax.legend()
-            plt.draw()
-            plt.pause(0.1)
+            # Обновление графиков визуализации
+            # ax1.clear()
+            # ax1.plot(self.iterations, self.distances, label='Best Distance')
+            # ax1.set_xlabel('Iteration')
+            # ax1.set_ylabel('Distance')
+            # ax1.set_title('Ant Algorithm Optimization')
+            # ax1.legend()
 
-        plt.ioff()
+            
+
+            # ax2.clear()
+            # ax2.plot(self.iterations, min_paths, label='Minimum Path Length', color='green')
+            # ax2.set_xlabel('Iteration')
+            # ax2.set_ylabel('Minimum Path Length')
+            # ax2.set_title('Minimum Path Length Over Iterations')
+            # ax2.legend()
+
+            # plt.draw()
+            # plt.pause(0.1)
+        ax1.clear()
+        ax1.plot(self.iterations, self.distances, label='Best Distance')
+        ax1.set_xlabel('Iteration')
+        ax1.set_ylabel('Distance')
+        ax1.set_title('Ant Algorithm Optimization')
+        ax1.legend()
+
+        
+
+        ax2.clear()
+        ax2.plot(self.iterations, min_paths, label='Minimum Path Length', color='green')
+        ax2.set_xlabel('Iteration')
+        ax2.set_ylabel('Minimum Path Length')
+        ax2.set_title('Minimum Path Length Over Iterations')
+        ax2.legend()
+        best_path_probabilities = [self.calculate_probability_of_best_path(best_cycle, pheromones) for pheromones in all_pheromones]
+        ax3.clear()
+        ax3.plot(self.iterations, best_path_probabilities, label='Ratio of Best Path', color='orange')
+        ax3.set_xlabel('Iteration')
+        ax3.set_ylabel('Ratio')
+        ax3.set_title('Ratio of Choosing Best Path')
+        ax3.legend()
+        # plt.ioff()
         plt.show()
 
         return best_cycle, best_distance
+
+
+
+    def calculate_probability_of_best_path(self, best_cycle, pheromones):
+        """
+        Расчет вероятности выбора лучшего найденного пути.
+        :param best_cycle: Лучший маршрут.
+        :return: Вероятность выбора лучшего маршрута.
+        """
+        if not best_cycle:
+            return 0
+        
+        total_pheromone = sum(
+            pheromones[(best_cycle[i], best_cycle[i + 1])] 
+            for i in range(len(best_cycle) - 1)
+        )
+        
+        if total_pheromone == 0:
+            return 0
+        
+        probability = total_pheromone / sum(pheromones.values())
+        
+        return probability
 
     def construct_solution(self):
         """
@@ -167,28 +235,34 @@ class AntAlgorithm:
         chosen = random.choices(neighbours, weights=probabilities)[0][0] #[(node_A, 10), (node_B, 20), (node_C, 15)] [0.2, 0.5, 0.3]
         return chosen
 
-    def update_pheromones(self, all_cycles):
+    def evaporate_pheromones(self):
         """
-        Обновление уровня феромонов на всех ребрах.
-        :param all_cycles: Список маршрутов и их длин, построенных муравьями.
+        Испарение феромонов.
         """
         # Испарение феромонов
         for edge in self.pheromones.keys():
             self.pheromones[edge] *= (1 - self.decay)
 
-        # Добавление феромонов для маршрутов
-        for cycle, distance in all_cycles:
-            if cycle and len(cycle) == len(self.graph.nodes) + 1:  # Проверка на замкнутый цикл
-                pheromone_deposit = 1 / distance if distance != 0 else 0  # Количество феромона зависит от длины маршрута
-                unique_edges = set()
 
-                # Обновление феромонов только для уникальных ребер
-                for i in range(len(cycle) - 1):
-                    edge = (cycle[i], cycle[i + 1])
-                    
-                    if edge not in unique_edges and edge in self.pheromones:
-                        self.pheromones[edge] += pheromone_deposit
-                        unique_edges.add(edge)
+    def update_pheromones(self, cycle, distance):
+        """
+        Обновление уровня феромонов на всех ребрах пути.
+        :param cycle: Маршрут, построенный муравьем.
+        :param distance: Длина маршрута, построенного муравьем.
+        """
+        # Добавление феромонов для маршрутов
+        # for cycle, distance in all_cycles:
+        if cycle and len(cycle) == len(self.graph.nodes) + 1:  # Проверка на замкнутый цикл
+            pheromone_deposit = 1 / distance if distance != 0 else 0  # Количество феромона зависит от длины маршрута
+            unique_edges = set()
+
+            # Обновление феромонов только для уникальных ребер
+            for i in range(len(cycle) - 1):
+                edge = (cycle[i], cycle[i + 1])
+                
+                if edge not in unique_edges and edge in self.pheromones:
+                    self.pheromones[edge] += pheromone_deposit
+                    unique_edges.add(edge)
 
     @classmethod
     def from_config(cls, config_file: str, graph: Graph):
